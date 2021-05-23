7bfaf6917ee2d1c85a6fea1a1a72752e22662c28
Francesco <kekkorubino@msn.com>
2021-05-23 17:47:03 +0100

    Consuming API

---
+++ ConsumingWebAPI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ConsumingWebAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var baseUrl = "https://localhost:44372/";
+            CallEndpoint(baseUrl, "api/products");
+            CallEndpoint(baseUrl, "api/products/2");
+            CallEndpoint(baseUrl, "api/products/categories");
+            CallEndpoint(baseUrl, "api/products/productbycategory/1");
+        }
+
+        public static void CallEndpoint(string baseUrl, string endpoint)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44372/");
+                var response = client.GetAsync(endpoint).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseString = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine(responseString);
+                }
+            }
+        }
+    }
+}
